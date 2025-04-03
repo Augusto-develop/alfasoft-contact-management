1edae791d184455753c9c8483e477bbc4a1ff265
Augusto-develop <augustogomes0822@gmail.com>
2025-04-03 18:06:24 -0300

    build (vite): cors

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        https: true,
-        host: 'augustogomes-lv.recruitment.alfasoft.pt',
-        port: 5175,
-        cors: {
-            origin: 'https://augustogomes-lv.recruitment.alfasoft.pt',
-            methods: ['GET', 'POST', 'PUT', 'DELETE'],
-            allowedHeaders: ['Content-Type', 'Authorization'],
-        }
-    },
     plugins: [
         laravel({
             input: [
